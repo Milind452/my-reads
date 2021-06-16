4a143e6465843b37c6cbd4a0a260c7e829e60f9a
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-06-16 22:14:32 +0530

    Move Bookshelf component to its seperate file

---
+++ src/Bookshelf.js
@@ -0,0 +1,60 @@
+const Bookshelf = (props) => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{props.title}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {props.books.map((book) => {
+                        console.log(book);
+                        return (
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div
+                                            className="book-cover"
+                                            style={{
+                                                width: "148px",
+                                                height: "193px",
+                                                backgroundImage: `url(${book.imageLinks.thumbnail})`,
+                                            }}
+                                        ></div>
+                                        <div className="book-shelf-changer">
+                                            <select>
+                                                <option
+                                                    value="move"
+                                                    disabled=""
+                                                >
+                                                    Move to...
+                                                </option>
+                                                <option value="currentlyReading">
+                                                    Currently Reading
+                                                </option>
+                                                <option value="wantToRead">
+                                                    Want to Read
+                                                </option>
+                                                <option value="read">
+                                                    Read
+                                                </option>
+                                                <option value="none">
+                                                    None
+                                                </option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">
+                                        {book.title}
+                                    </div>
+                                    <div className="book-authors">
+                                        {book.authors}
+                                    </div>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ol>
+            </div>
+        </div>
+    );
+};
+
+export default Bookshelf;
--- src/ListBook.js
+++ src/ListBook.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import * as BooksAPI from "./BooksAPI";
+import Bookshelf from "./Bookshelf";
 
 class ListBooks extends React.Component {
     constructor(props) {
@@ -59,64 +60,5 @@ class ListBooks extends React.Component {
         );
     }
 }
-
-const Bookshelf = (props) => {
-    return (
-        <div className="bookshelf">
-            <h2 className="bookshelf-title">{props.title}</h2>
-            <div className="bookshelf-books">
-                <ol className="books-grid">
-                    {props.books.map((book) => {
-                        console.log(book);
-                        return (
-                            <li key={book.id}>
-                                <div className="book">
-                                    <div className="book-top">
-                                        <div
-                                            className="book-cover"
-                                            style={{
-                                                width: "148px",
-                                                height: "193px",
-                                                backgroundImage: `url(${book.imageLinks.thumbnail})`,
-                                            }}
-                                        ></div>
-                                        <div className="book-shelf-changer">
-                                            <select>
-                                                <option
-                                                    value="move"
-                                                    disabled=""
-                                                >
-                                                    Move to...
-                                                </option>
-                                                <option value="currentlyReading">
-                                                    Currently Reading
-                                                </option>
-                                                <option value="wantToRead">
-                                                    Want to Read
-                                                </option>
-                                                <option value="read">
-                                                    Read
-                                                </option>
-                                                <option value="none">
-                                                    None
-                                                </option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div className="book-title">
-                                        {book.title}
-                                    </div>
-                                    <div className="book-authors">
-                                        {book.authors}
-                                    </div>
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ol>
-            </div>
-        </div>
-    );
-};
 
 export default ListBooks;
