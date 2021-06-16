71452eff41901775a3354fd61d22c4818621b1ff
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-06-16 19:52:28 +0530

    Add close link and search bar to addbooks component

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class AddBooks extends React.Component {
     constructor(props) {
@@ -7,6 +8,20 @@ class AddBooks extends React.Component {
     render() {
         return (
             <div className="add-books">
+                <div className="search-bar">
+                    <div className="close-search">
+                        <Link to="/" className="btn">
+                            Close
+                        </Link>
+                    </div>
+                    <div className="search-input-wrapper">
+                        <input
+                            className="search-input"
+                            type="text"
+                            placeholder="Search by title or author"
+                        ></input>
+                    </div>
+                </div>
                 <h1>Add Book</h1>
             </div>
         );
