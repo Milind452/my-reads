e5c468d74484b3b3fb268994fd3d217b08610725
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-06-16 19:09:23 +0530

    Make basic ListBooks component

---
+++ src/ListBook.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ListBooks extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="list-books">
+                <h1>Show Book</h1>
+            </div>
+        );
+    }
+}
+
+export default ListBooks;
