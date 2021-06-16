067bd721dddba92fe45679d84601dc9c2eaef61a
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-06-16 19:10:14 +0530

    Make basic AddBooks component

---
+++ src/AddBook.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class AddBooks extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="add-books">
+                <h1>Add Book</h1>
+            </div>
+        );
+    }
+}
+
+export default AddBooks;
