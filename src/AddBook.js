3d0b694d04080ad769024f60b138b51914457ab1
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-06-17 02:53:16 +0530

    Add new books by searching through api call

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import Bookshelf from "./Bookshelf";
 
 class AddBooks extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            query: "",
+            books: [],
+        };
+        this.updateQuery = this.updateQuery.bind(this);
     }
+
+    updateQuery(query) {
+        this.setState(() => ({
+            query: query.trim(),
+        }));
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.query !== this.state.query) {
+            BooksAPI.search(this.state.query).then((books) => {
+                this.setState(() => ({
+                    books: books,
+                }));
+            });
+        }
+    }
+
     render() {
         return (
             <div className="add-books">
@@ -19,10 +43,19 @@ class AddBooks extends React.Component {
                             className="search-input"
                             type="text"
                             placeholder="Search by title or author"
+                            value={this.state.query}
+                            onChange={(event) => {
+                                this.updateQuery(event.target.value);
+                            }}
                         ></input>
                     </div>
                 </div>
-                <h1>Add Book</h1>
+                <div className="search-result">
+                    <Bookshelf
+                        books={this.state.books}
+                        moveBook={this.moveBook}
+                    />
+                </div>
             </div>
         );
     }
